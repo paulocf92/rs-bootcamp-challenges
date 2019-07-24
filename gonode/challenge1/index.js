e5d556836b4a47ad0e62904fb317a2b5e1cf7754
Paulo Filho <pcf.feat@gmail.com>
2019-07-24 08:33:59 -0400

    feat: listings route

--- gonode/challenge1/index.js
+++ gonode/challenge1/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => res.send('Hello!'));
+server.use(express.json());
+
+// in-memory projects storage
+const projects = [
+   { id: 0, title: 'Trip', tasks: ['Save up money', 'Set up a date', 'Make flight reservation', 'Make hotel reservation', 'Do it!']},
+   { id: 1, title: 'Finish node project', tasks: ['Evaluate dependencies', 'Finish core app', 'Choose tools to handle routing', 'Implement them']},
+];
+
+// return all projects
+server.get('/projects', (req, res) => res.json(projects));
 
 server.listen(3000);
