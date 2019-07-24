442b1613bc752b96ac2887013671c47697c69367
Paulo Filho <pcf.feat@gmail.com>
2019-07-24 09:06:04 -0400

    feat: create route

--- gonode/challenge1/index.js
+++ gonode/challenge1/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
@@ -6,11 +6,39 @@ server.use(express.json());
 
 // in-memory projects storage
 const projects = [
-   { id: 0, title: 'Trip', tasks: ['Save up money', 'Set up a date', 'Make flight reservation', 'Make hotel reservation', 'Do it!']},
-   { id: 1, title: 'Finish node project', tasks: ['Evaluate dependencies', 'Finish core app', 'Choose tools to handle routing', 'Implement them']},
+  {
+    id: "0",
+    title: "Trip",
+    tasks: [
+      "Save up money",
+      "Set up a date",
+      "Make flight reservation",
+      "Make hotel reservation",
+      "Do it!"
+    ]
+  },
+  {
+    id: "1",
+    title: "Finish node project",
+    tasks: [
+      "Evaluate dependencies",
+      "Finish core app",
+      "Choose tools to handle routing",
+      "Implement them"
+    ]
+  }
 ];
 
 // return all projects
-server.get('/projects', (req, res) => res.json(projects));
+server.get("/projects", (req, res) => res.json(projects));
+
+// register a new project
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({ id, title, tasks: [] });
+
+  return res.json(projects);
+});
 
 server.listen(3000);
