4445eed35ab3ec29cfe1a5c80ebdb550a0acca51
Paulo Filho <pcf.feat@gmail.com>
2019-09-27 15:37:43 -0400

    feat: header component

---
+++ goreact/challenge1/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => (
+  <ul class="menu">
+    <li class="logo">facebook</li>
+    <li class="profile">
+      Meu perfil <i class="material-icons">account_circle</i>
+    </li>
+  </ul>
+);
+
+export default Header;
