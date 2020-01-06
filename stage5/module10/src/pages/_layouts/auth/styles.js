60fc8dbd9519b29e96c56c6cdc454279f8e86cd0
Paulo Filho <pcf.feat@gmail.com>
2020-01-06 15:51:42 -0400

    feat: global styles

--- stage5/module10/src/pages/_layouts/auth/styles.js
+++ stage5/module10/src/pages/_layouts/auth/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #f5f5f5;
+  background: #ee4d64;
   display: flex;
   justify-content: center;
   align-items: center;
