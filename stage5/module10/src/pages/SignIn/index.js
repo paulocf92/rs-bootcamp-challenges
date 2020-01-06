a6a331bacc9db9065de93b963fa65558d93789c9
Paulo Filho <pcf.feat@gmail.com>
2020-01-06 17:03:57 -0400

    feat: sign-in page with rocketseat's unform; validation with yup

--- stage5/module10/src/pages/SignIn/index.js
+++ stage5/module10/src/pages/SignIn/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('E-mail obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve ter no mínimo 6 caracteres')
+    .required('Senha obrigatória'),
+});
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  function handleSubmit(data) {
+    console.log(data); // eslint-disable-line
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Gympoint" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          label="seu e-mail"
+          type="email"
+          placeholder="exemplo@email.com"
+        />
+        <Input
+          name="password"
+          label="sua senha"
+          type="password"
+          placeholder="********"
+        />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
