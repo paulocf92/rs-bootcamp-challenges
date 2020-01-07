99753105bbae057d87275b77ac770a6b6531956f
Paulo Filho <pcf.feat@gmail.com>
2020-01-07 14:57:29 -0400

    feat: authentication with redux and persistence with redux-persist; fix: added cors to backend

--- stage5/module10/src/pages/SignIn/index.js
+++ stage5/module10/src/pages/SignIn/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -14,8 +17,11 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.log(data); // eslint-disable-line
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
@@ -35,7 +41,9 @@ export default function SignIn() {
           placeholder="********"
         />
 
-        <button type="submit">Entrar no sistema</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Entrar no sistema'}
+        </button>
       </Form>
     </>
   );
--- stage5/module10/src/store/modules/auth/sagas.js
+++ stage5/module10/src/store/modules/auth/sagas.js
@@ -1,3 +1,54 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess, signInFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/students');
+  } catch (err) {
+    toast.error('Falha na autenticação! Verifique seus dados.');
+    yield put(signInFailure());
+  }
+}
+
+/**
+  Each time redux-persist reloads data, apply auth token to our api header's
+  authorization so that each request is successfully authenticated.
+  This is possible by listening to redux-persist's persist/REHYDRATE action.
+*/
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
