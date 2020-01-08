2d8682642803a23a06e767f1c6366f060416bab1
Paulo Filho <pcf.feat@gmail.com>
2020-01-08 17:17:52 -0400

    feat: student list main layout and search feature

--- stage5/challenge10/src/pages/StudentList/index.js
+++ stage5/challenge10/src/pages/StudentList/index.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, StudentTable, ListHeader } from './styles';
 
 export default function StudentList() {
-  return <h1>StudentList</h1>;
+  const [students, setStudents] = useState([]);
+  const [searchedStudents, setSearchedStudents] = useState([]);
+
+  useEffect(() => {
+    async function loadStudents() {
+      const response = await api.get('/students');
+
+      setStudents(response.data);
+      setSearchedStudents(response.data);
+    }
+
+    loadStudents();
+  }, [setStudents, setSearchedStudents]);
+
+  function handleOnChange(e) {
+    const searched = students.filter(student =>
+      student.name.toUpperCase().startsWith(e.target.value.toUpperCase()),
+    );
+    setSearchedStudents(searched);
+  }
+
+  return (
+    <Container>
+      <ListHeader>
+        <h2>Gerenciando alunos</h2>
+        <div>
+          <button type="button">CADASTRAR</button>
+          <input
+            type="text"
+            name="student"
+            id="student"
+            placeholder="Buscar aluno"
+            onChange={handleOnChange}
+          />
+        </div>
+      </ListHeader>
+
+      <div>
+        <StudentTable>
+          <thead>
+            <tr>
+              <th>NOME</th>
+              <th>E-MAIL</th>
+              <th>IDADE</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchedStudents.map(student => (
+              <tr key={student.id}>
+                <td>{student.name}</td>
+                <td>{student.email}</td>
+                <td>{student.age}</td>
+                <td>
+                  <button type="button">editar</button>
+                  <button type="button">apagar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </StudentTable>
+      </div>
+    </Container>
+  );
 }
---
+++ stage5/challenge10/src/pages/StudentList/styles.js
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  width: 100%;
+  margin: auto 60px;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    color: #444;
+  }
+
+  > div {
+    padding: 20px 10px 10px;
+    margin-top: 20px;
+    color: #666;
+    background: #fff;
+    border-radius: 4px;
+  }
+`;
+
+export const StudentTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  color: #666;
+
+  thead th {
+    color: #444;
+    text-align: left;
+    padding: 5px 12px;
+
+    &:nth-child(3) {
+      text-align: center;
+    }
+  }
+
+  tbody tr:not(:last-child) td {
+    border-bottom: 1px solid #eee;
+  }
+
+  tbody tr {
+    transition: background 0.5s;
+
+    &:hover {
+      background: ${darken(0.08, '#fff')};
+    }
+
+    td {
+      padding: 12px;
+
+      &:first-child {
+        min-width: 250px;
+      }
+
+      &:nth-child(3) {
+        text-align: center;
+      }
+
+      &:nth-child(4) {
+        text-align: right;
+      }
+
+      button {
+        border: 0;
+        background: none;
+        padding: 0 10px;
+        transition: color 0.5s;
+
+        &:first-child {
+          color: #4d85ee;
+
+          &:hover {
+            color: ${darken(0.4, '#4d85ee')};
+          }
+        }
+
+        &:last-child {
+          color: #de3b3b;
+
+          &:hover {
+            color: ${darken(0.4, '#de3b3b')};
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const ListHeader = styled.header`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+
+  div {
+    button {
+      border: 0;
+      background: #ee4d64;
+      border-radius: 4px;
+      padding: 10px 15px;
+      font-weight: bold;
+      color: #fff;
+      transition: background 0.5s;
+
+      &:hover {
+        background: ${darken(0.2, '#ee4d64')};
+      }
+    }
+
+    input {
+      margin-left: 20px;
+      padding: 10px 25px;
+      border-radius: 4px;
+      border: 1px solid #ddd;
+      color: #999;
+
+      &::placeholder {
+        color: #bbb;
+      }
+    }
+  }
+`;
